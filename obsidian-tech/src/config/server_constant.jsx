8c89dc27bde9c9ee86637a2a7631782307e38b8a
rodrigo <rodrigoaragon1977@gmail.com>
2024-03-06 11:18:52 -0300

    creando entornos flexibles

--- obsidian-tech/src/config/server_constant.jsx
+++ obsidian-tech/src/config/server_constant.jsx
@@ -1,6 +1,7 @@
 const Puerto = {
-  URL_LOCAL: 'http://localhost:1000/api',
-  URL_GLOBAL: 'https://obsidian-database.onrender.com/api'
+  // URL_LOCAL: 'http://localhost:1000/api',
+  // URL_GLOBAL: 'https://obsidian-database.onrender.com/api'
+  URL_GLOBAL: import.meta.env.VITE_API_URL
 }
 
 export { Puerto }
