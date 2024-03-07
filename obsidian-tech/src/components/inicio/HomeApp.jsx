b9e74dcea01bc515df894a6c4506825dec79da14
Fernando <fer3443@gmail.com>
2024-03-07 13:49:56 -0300

    [refactoring] Estilos en HomeApp

--- obsidian-tech/src/components/inicio/HomeApp.jsx
+++ obsidian-tech/src/components/inicio/HomeApp.jsx
@@ -17,41 +17,35 @@ export const HomeApp = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="homeGralContainer">
-					<div className="circleBg one"></div>
-					<div className="circleBg two"></div>
-					<div className="circleBg three"></div>
-					<div className="homeContainer">
-          <div className="introContainer">
-            <div className="titleContainer">
-              <h3>
-                Los Mejores Accesorios Para
-                <span className="titleSpan"> Un Jugador Profesional</span>.
-              </h3>
-              <p>
-                Bienvenido a Obsidian Tech, tu destino definitivo para los
-                mejores accesorios de videojuegos. En Obsidian Tech, nos
-                apasiona proporcionarte una experiencia de juego excepcional y
-                elevar tu rendimiento al siguiente nivel.
-              </p>
-              <h5>Podes ver el catalogo completo.</h5>
-              <p>
-                Nuestro amplio catálogo de accesorios de alta calidad está
-                diseñado pensando en los jugadores más exigentes. Desde
-                auriculares y teclados hasta controladores y mousepad, cada
-                producto de Obsidian Tech ha sido cuidadosamente seleccionado
-                para brindarte el máximo rendimiento y comodidad.
-              </p>
-              <div className="buttonLinkContainer">
-                <Link to={'/catalogo'} className="button">
-                  Ver Catalogo
-                </Link>
-              </div>
+        <main>
+          <section className="section section-intro">
+            <div className="container-intro container grid">
+              <article className="grid">
+                <h1 className="title-intro">
+                  Los Mejores Accesorios Para un
+                  <span className="titleSpan">Jugador Profesional</span>
+                </h1>
+                <p>
+                  Bienvenido a Obsidian Tech, tu destino definitivo para los
+                  mejores accesorios de videojuegos. En Obsidian Tech, nos
+                  apasiona proporcionarte una experiencia de juego excepcional y
+                  elevar tu rendimiento al siguiente nivel.
+                  Nuestro amplio catálogo de accesorios de alta calidad está
+                  diseñado pensando en los jugadores más exigentes. Desde
+                  auriculares y teclados hasta controladores y mousepad, cada
+                  producto de Obsidian Tech ha sido cuidadosamente seleccionado
+                  para brindarte el máximo rendimiento y comodidad.
+                </p>
+                <div className="buttonLinkContainer">
+                  <Link to={"/catalogo"} className="btn1">
+                    Ver Catalogo
+                  </Link>
+                </div>
+              </article>
             </div>
-          </div>
+          </section>
           <ProductGrid />
-					</div>
-        </div>
+        </main>
       )}
     </>
   );
